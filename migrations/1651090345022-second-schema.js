90dd727551fb7b53d9abf8bbbccb5f8514469e64
Mateus dos Santos <loiolamateus7@gmail.com>
2022-04-27 17:26:58 -0300

    update migrations

--- migrations/1651090345022-second-schema.js
+++ migrations/1651090345022-second-schema.js
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+const { MigrationInterface, QueryRunner } = require('typeorm');
 
 module.exports = class secondSchema1651090345022 {
   name = 'secondSchema1651090345022';
